b5a29b0d3b6b226c0d68afce56f2a513c1f4719e
david <davidcondori99@gmail.com>
2025-07-22 12:14:51 -0500

    feat: add fontsource dependencies and update main.scss imports for typography

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,13 @@ import { store } from './app/store.ts'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/index.ts'
 
+// import '@fontsource/inter/400.css'
+// import '@fontsource/inter/600.css'
+// import '@fontsource/inter/700.css'
+// import '@fontsource/bebas-neue/400.css'
+
+import './styles/main.scss'
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Provider store={store}>
